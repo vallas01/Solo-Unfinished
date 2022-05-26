78588fbc4aab7be940e59ace70990ad9d8ffa5e9
vallas01 <vallasac@gmail.com>
2022-05-26 13:22:08 -0400

    Phase 2

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -19,24 +20,55 @@ if (!isProduction) {
     app.use(cors());
   }
 
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
+// helmet helps set a variety of headers to better secure your app
+app.use(
     helmet.crossOriginResourcePolicy({
       policy: "cross-origin"
     })
-  );
+);
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
+// Set the _csrf token and create req.csrfToken method
+app.use(
     csurf({
-      cookie: {
+        cookie: {
         secure: isProduction,
         sameSite: isProduction && "Lax",
         httpOnly: true
-      }
+        }
     })
-  );
+);
 
-  app.use(routes); // Connect all the routes
+app.use(routes); // Connect all the routes
 
-  module.exports = app;
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+  // Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
+
+module.exports = app;
