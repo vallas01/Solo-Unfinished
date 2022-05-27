722dfecdea6750d5ddbec8e4f96c0c7c20db6f64
vallas01 <vallasac@gmail.com>
2022-05-26 23:27:29 -0400

    Done frontend

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -23,7 +23,20 @@ export const restoreUser = () => async dispatch => {
     return response;
   };
 
-
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 
 
 export const login = (user) => async (dispatch) => {
