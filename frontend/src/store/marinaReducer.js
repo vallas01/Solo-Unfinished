4c14dcc36e81ffd213e30abc963b3bbf0b8566bb
vallas01 <vallasac@gmail.com>
2022-05-28 14:08:25 -0400

    working on reducer

--- frontend/src/store/marinaReducer.js
+++ frontend/src/store/marinaReducer.js
@@ -1,30 +1,77 @@
 import marinas from '../data/data.json';
 
-const LOAD_MARINAS = 'marina/loadMarinas';
-const ADD_MARINA = 'marina/addMarina';
+/*-------- TYPES -------*/
+export const LOAD_MARINAS = 'marina/loadMarinas';
+export const ADD_MARINA = 'marina/addMarina';
+// export const REMOVE_MARINA = 'marina/removeMarina';
+// export const UPDATE_MARINA = 'marina/updateMarina'
 
-export const loadMarinas = () => {
+/*-------- ACTIONS -------*/
+const load = (marinas) => {
   return {
     type: LOAD_MARINAS,
     marinas
   };
 };
 
-
-export const addMarina = (marina) => {
+const add = (marina) => {
   return {
     type: ADD_MARINA,
     marina
   };
 };
 
+// const remove = () => {
+//     return {
+//       type: REMOVE_MARINA,
+//     };
+// };
 
-const initialState = { entries: [], isLoading: true };
+// const update = () => {
+//     return {
+//       type: UPDATE_MARINA,
+//     };
+// };
+
+/*-------- SELECTORS -------*/
+
+export const loadMarinas = () => async (dispatch) => {
+    const response = await fetch('/api/marinas', {
+      method: 'GET'
+    });
+    if (response.ok) {
+        const marinas = await response.json();
+        dispatch(load(marinas));
+    }
+};
+
+export const addMarina = (marina) => async (dispatch) =>{
+    const response = await fetch ('/api/marinas', {
+        method: 'POST',
+        headers: {"ContentType": 'application/json',
+    },
+        body: JSON.stringify(marina)
+    })
+    if(response.ok){
+        const marina = await response.json();
+        dispatch(create(marina));
+        return marina;
+    }
+}
+
+
+
+/*-------- REDUCER -------*/
+const initialState = {};
 
 const marinaReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_MARINAS:
-      return { ...state, entries: [...action.marinas] };
+      const newMarina = {};
+      action.marinas.marina.forEach(marina => {
+          newMarina[marina.id] = marina
+      })
+      return { ...state,...newMarina};
 
     case ADD_MARINA:
       return { ...state, entries: [...state.entries, action.marina] };
