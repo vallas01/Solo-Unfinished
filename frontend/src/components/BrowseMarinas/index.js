8cc214cc6720cca3ec4df5ac980332a3963bfb65
vallas01 <vallasac@gmail.com>
2022-05-29 10:30:40 -0400

    reducer 2nd try

---
+++ frontend/src/components/BrowseMarinas/index.js
@@ -0,0 +1,19 @@
+import { useEffect  } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { listOfAllBusinesses } from '../../store/businesses';
+import { Footer } from '../Footer';
+import './BrowseBusiness.css'
+
+
+
+export const BrowseBusinesses = () => {
+
+    const dispatch = useDispatch();
+    const listOfBusinesses = useSelector(state => {
+        return state.businesses.list
+    });
+
+    useEffect(() => {
+        dispatch(listOfAllBusinesses())
+    }, [dispatch]);
--- frontend/src/store/marinaReducer.js
+++ frontend/src/store/marinaReducer.js
@@ -1,77 +1,106 @@
-// import marinas from '../data/data.json';
+import { csrfFetch } from "./csrf";
 
-/*-------- TYPES -------*/
-export const LOAD_MARINAS = 'marina/loadMarinas';
-export const ADD_MARINA = 'marina/addMarina';
-// export const REMOVE_MARINA = 'marina/removeMarina';
-// export const UPDATE_MARINA = 'marina/updateMarina'
+const LIST_MARINAS = 'marina/listMarinas';
+const LIST_1_MARINA = 'marina/listOneMarina'
+const ADD_MARINA = 'marina/addMarina';
+const REMOVE_MARINA = 'marina/removeMarina';
+const UPDATE_MARINA = 'marina/updateMarina'
 
-/*-------- ACTIONS -------*/
-const load = (marinas) => {
-  return {
-    type: LOAD_MARINAS,
-    marinas
-  };
-};
 
-const add = (marina) => {
-  return {
-    type: ADD_MARINA,
-    marina
-  };
-};
-
-// const remove = () => {
-//     return {
-//       type: REMOVE_MARINA,
-//     };
-// };
-
-// const update = () => {
-//     return {
-//       type: UPDATE_MARINA,
-//     };
-// };
+const listMarinas = list => ({
+  type: LIST_MARINAS,
+  list
+});
+const listOneMarina = details => ({
+  type: LIST_1_MARINA,
+  details
+});
+const addMarinas = details => ({
+  type: ADD_MARINA,
+  details
+})
+const removeMarina = id => ({
+  type: REMOVE_MARINA,
+  id
+});
+const updateMarina = updatedMarina => ({
+  type: UPDATE_MARINA,
+  updatedMarina
+});
 
 /*-------- SELECTORS -------*/
 
-export const loadMarinas = () => async (dispatch) => {
-    const response = await fetch('/api/marinas', {
-      method: 'GET'
-    });
+export const listAllMarinas = () => async (dispatch) => {
+    const response = await csrfFetch(`/api/marinas`);
     if (response.ok) {
-        const marinas = await response.json();
-        dispatch(load(marinas));
+        const list = await response.json();
+        dispatch(listMarinas(list));
     }
 };
 
-export const addMarina = (marina) => async (dispatch) =>{
-    const response = await fetch ('/api/marinas', {
-        method: 'POST',
-        headers: {"ContentType": 'application/json',
-    },
-        body: JSON.stringify(marina)
-    })
+export const getOneMarina = (marinaId) => async (dispatch) =>{
+    const response = await csrfFetch (`/api/marinas/${marinaId}`);
+
     if(response.ok){
-        const marina = await response.json();
-        dispatch(add(marina));
-        return marina;
+        const details = await response.json();
+        dispatch(listOneMarina(details));
     }
-}
+};
+
+export const createMarina = (details) => async dispatch => {
+  const response = await csrfFetch('/api/marinas', {
+    method: 'POST',
+    body: JSON.stringify(details)
+  })
+  if(response.ok){
+    const newMarina = await response.json()
+    dispatch(addMarinas(newMarina))
+    return newMarina;
+  }
+};
+
+export const updateMarinaDetails = (marinaDetails) => async dispatch => {
+  const response = await csrfFetch(`/api/marinas/${marinaDetails.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(marinaDetails)
+  })
+  if(response.ok){
+    const updatedMarina = await response.json()
+    dispatch(updateMarina(updatedMarina))
+    return updatedMarina;
+  }
+};
+
+export const deleteMarina = (marinaId) => async dispatch => {
+  const response = await csrfFetch(`/api/marinas/${marinaId}`, {
+    method: 'DELETE',
+    body: JSON.stringify({marinaId})
+  })
+  if(response.ok){
+      const allMarinas = await response.json();
+      dispatch(listAllMarinas(allMarinas));
+      return 'marina deleted';
+  }
+};
 
 
 
 /*-------- REDUCER -------*/
-const initialState = {};
+const initialState = {list:[], currentMarina:[]};
 
 const marinaReducer = (state = initialState, action) => {
+  let newState;
+  
   switch (action.type) {
-    case LOAD_MARINAS:
-      const newMarina = {};
-      action.marinas.marina.forEach(marina => {
-          newMarina[marina.id] = marina
-      })
-      return { ...state,...newMarina};
+
+    case LIST_MARINAS: {
+      const allMarinas = action.list;
+      newState = {
+        list: allMarinas,
+        currentMarina:[]
+      }
+      return newState;
+    }
 
     case ADD_MARINA:
       return { ...state, entries: [...state.entries, action.marina] };
