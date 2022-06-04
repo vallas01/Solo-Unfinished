9bd58299f894f73b895374d39477fcee7f1c650d
vallas01 <vallasac@gmail.com>
2022-06-04 14:15:42 -0400

    splashpage image

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -7,10 +7,13 @@ function SplashPage() {
 
 
     return (
-        <>
+        <div className='splash-container'>
             <div className='Title'>Find yourself on the water</div>
             <p className='Title-tag'>Discover and book a slip, restaurant dock, mooring and enjoy.</p>
-        </>
+            <div className='img-container'>
+                <img className='img-sail' src= {require("./AstonMartin.jpg")}  alt='sailboat'></img>
+            </div>
+        </div>
     );
 }
 
