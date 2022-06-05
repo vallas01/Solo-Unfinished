2830509de8cc6624db3e54803ac73de73b8b1146
vallas01 <vallasac@gmail.com>
2022-06-05 02:05:50 -0400

    buggy

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,11 +3,14 @@ import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
 import '../button.css'
+import { useHistory} from 'react-router-dom';
 
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
+  const history = useHistory();
+
 
   const openMenu = () => {
     if (showMenu) return;
@@ -29,6 +32,7 @@ function ProfileButton({ user }) {
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    history.pushState('/');
   };
 
   return (
