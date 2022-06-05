1e64d2625ca50b19fadbfce5fbf06dc20e4041ab
vallas01 <vallasac@gmail.com>
2022-06-05 01:43:49 -0400

    trying original ProfileButton

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,5 +1,4 @@
-import React from "react";
-// import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
@@ -8,43 +7,42 @@ import '../button.css'
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
-  // const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-  // const openMenu = () => {
-  //   if (showMenu) return;
-  //   setShowMenu(true);
-  // };
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
-  // useEffect(() => {
-  //   if (!showMenu) return;
+  useEffect(() => {
+    if (!showMenu) return;
 
-  //   const closeMenu = () => {
-  //     setShowMenu(false);
-  //   };
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
 
-  //   document.addEventListener('click', closeMenu);
+    document.addEventListener('click', closeMenu);
 
-  //   return () => document.removeEventListener("click", closeMenu);
-  // }, [showMenu]);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
-    window.location = '/';
   };
 
   return (
     <>
-      {/* <button className="marina-btn" onClick={openMenu}>User</button>
+      <button className="marina-btn" onClick={openMenu}>User</button>
       {showMenu && (
         <ul className="profile-dropdown">
           <li>{user.username}</li>
           <li>{user.email}</li>
-          <li> */}
+          <li>
             <button className="logout-btn" onClick={logout}>Logout</button>
-          {/* </li>
+          </li>
         </ul>
-      )} */}
+      )}
     </>
   );
 }
