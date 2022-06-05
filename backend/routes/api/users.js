506275e6418148a20e7b35eb5d5d11fab36f8c6b
vallas01 <vallasac@gmail.com>
2022-06-05 09:41:09 -0400

    working on bcrypt issue

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
 
-const router = express.Router();
 
 const validateSignup = [
     check('email')
