c5b753a3a701f1ad22e784fc709dd1ff1a20e7ca
vallas01 <vallasac@gmail.com>
2022-06-10 22:55:50 -0400

    login error found

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
+import './index.css';
+
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
 import App from './App';
-import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+
+import configureStore from './store';
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
@@ -13,6 +18,7 @@ if (process.env.NODE_ENV !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
